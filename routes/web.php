efdf5f1a32cf151abf782ffa30987ed18d8366e0
Eduardo Moreira (Moreira) <edmoreira043557@gmail.com>
2026-09-12 09:48:56 -0300

    feat(books): adiciona interface de usuario para o modulo de livros

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+// Homepage
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+
+Route::get('/books', function() {
+    return view('books.index');
+})->name('books.index');
